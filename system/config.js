6d5a6a54156ed3206f1c008e0133ce8781e29c29
SysSolutions <131492089+SysSolutionsPE@users.noreply.github.com>
2023-07-03 13:32:52 -0500

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,34 +1,34 @@
 const { Function, Scraper } = new (require('@neoxr/neoxr-js'))
 // Owner number
-global.owner = '6285887776722'
+global.owner = '51945463812'
 // Owner name
-global.owner_name = 'Wildan Izzudin'
+global.owner_name = 'SysSolutions Peru'
 // Database name (Default: database)
 global.database = 'database'
 // Maximum upload file size limit (Default : 100 MB)
-global.max_upload = 100
+global.max_upload = 1000
 // Delay for spamming protection (Default : 3 seconds)
 global.cooldown = 3
 // User Limitation (Default : 25)
-global.limit = 25
+global.limit = 2
 // Time to be temporarily banned and others (Default : 30 minutes)
 global.timer = 1800000
 // Symbols that are excluded when adding a prefix (Don't change it)
-global.evaluate_chars = ['=>', '~>', '<', '>', '$']
+global.evaluate_chars = ['=>', '~>', '<', '>', '$', '.']
 // Country code that will be automatically blocked by the system, when sending messages in private chat
-global.blocks = ['91', '92', '212']
+global.blocks = ['91', '92', '212', '62', '61']
 // Put target jid to forward friends story
 global.forwards = global.owner + '@c.us'
 // Get neoxr apikey by registering at https://api.neoxr.my.id
 global.Api = new (require('./neoxrApi'))(process.env.API_KEY)
 // Timezone (Default : Asia/Jakarta)
-global.timezone = 'Asia/Jakarta'
+global.timezone = 'America/Lima'
 // Bot version
 global.version = '2.2.2',
 // Bot name
-global.botname = `© neoxr-bot v${global.version} (Public Bot)`
+global.botname = `© SysSolutions-Bot v${global.version} (Public Bot)`
 // Footer text
-global.footer = 'ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ɴᴇᴏxʀ ッ'
+global.footer = 'SysSolutions Peru'
 // Scraper
 global.scrap = Scraper
 // Function
